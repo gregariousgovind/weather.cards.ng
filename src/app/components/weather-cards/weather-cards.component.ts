05e0ddef061973c85022a489b83dff3e478a368d
gregariousgovind <48236750+gregariousgovind@users.noreply.github.com>
2023-01-12 21:50:54 +0530

    1 file modified

--- src/app/components/weather-cards/weather-cards.component.ts
+++ src/app/components/weather-cards/weather-cards.component.ts
@@ -1,10 +1,9 @@
-import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'weather-cards',
   templateUrl: './weather-cards.component.html',
   styleUrls: ['./weather-cards.component.scss'],
-  encapsulation: ViewEncapsulation.None,
 })
 export class WeatherCards {
   @Input() cards;
