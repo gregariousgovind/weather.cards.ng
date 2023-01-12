49e65ff0bdfe80a90b83be41e312a0832edc7f0f
gregariousgovind <48236750+gregariousgovind@users.noreply.github.com>
2023-01-12 15:23:44 +0530

    MVP [Cards UI]

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,5 +16,45 @@ export class AppComponent {
         minValue: 61,
       },
     },
+    {
+      type: 'scale',
+      title: 'temperature',
+      data: {
+        value: 21,
+        valueType: 'temp', // configure to add °C after value
+        maxValue: 26,
+        minValue: 18,
+      },
+    },
+    {
+      type: 'status',
+      title: 'audio',
+      data: {
+        value: 2,
+        status: 'playing',
+        fieldName: 'total',
+        fieldValue: 5,
+      },
+    },
+    {
+      type: 'status',
+      title: 'displays',
+      data: {
+        value: 10,
+        valueOutOf: 16,
+        fieldName: 'on time',
+        fieldValue: '00:00',
+      },
+    },
+    {
+      type: 'status',
+      title: 'lighting',
+      data: {
+        value: 32,
+        status: 'on',
+        fieldName: 'total',
+        fieldValue: 43,
+      },
+    },
   ];
 }
