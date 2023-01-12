c6e9f492f12886b899dbdbb7bdd2927a694a5468
gregariousgovind <48236750+gregariousgovind@users.noreply.github.com>
2023-01-12 22:44:51 +0530

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+
+const cards = [
+  {
+    type: 'scale',
+    title: 'air quality',
+    data: {
+      value: 72,
+      max: {
+        value: 93,
+        icon: 'upArrow',
+        iconSize: 'medium',
+      },
+      min: {
+        value: 61,
+        icon: 'downArrow',
+        iconSize: 'medium',
+      },
+    },
+  },
+  {
+    type: 'scale',
+    title: 'temperature',
+    data: {
+      value: 21,
+      valueType: 'temp', // configure to add °C after value
+      max: {
+        value: 26,
+        icon: 'fire',
+        iconSize: 'small',
+      },
+      min: {
+        value: 18,
+        icon: 'snowflake',
+        iconSize: 'small',
+      },
+    },
+  },
+  {
+    type: 'status',
+    title: 'audio',
+    data: {
+      value: 2,
+      status: 'playing',
+      fieldName: 'total',
+      fieldValue: 5,
+    },
+  },
+  {
+    type: 'status',
+    title: 'displays',
+    data: {
+      value: 10,
+      valueOutOf: 16,
+      fieldName: 'on time',
+      fieldValue: '00:00',
+    },
+  },
+  {
+    type: 'status',
+    title: 'lighting',
+    data: {
+      value: 32,
+      status: 'on',
+      fieldName: 'total',
+      fieldValue: 43,
+    },
+  },
+];
 
 @Component({
   selector: 'my-app',
@@ -6,71 +74,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  cards = [
-    {
-      type: 'scale',
-      title: 'air quality',
-      data: {
-        value: 72,
-        max: {
-          value: 93,
-          icon: 'upArrow',
-          iconSize: 'medium',
-        },
-        min: {
-          value: 61,
-          icon: 'downArrow',
-          iconSize: 'medium',
-        },
-      },
-    },
-    {
-      type: 'scale',
-      title: 'temperature',
-      data: {
-        value: 21,
-        valueType: 'temp', // configure to add °C after value
-        max: {
-          value: 26,
-          icon: 'fire',
-          iconSize: 'small',
-        },
-        min: {
-          value: 18,
-          icon: 'snowflake',
-          iconSize: 'small',
-        },
-      },
-    },
-    {
-      type: 'status',
-      title: 'audio',
-      data: {
-        value: 2,
-        status: 'playing',
-        fieldName: 'total',
-        fieldValue: 5,
-      },
-    },
-    {
-      type: 'status',
-      title: 'displays',
-      data: {
-        value: 10,
-        valueOutOf: 16,
-        fieldName: 'on time',
-        fieldValue: '00:00',
-      },
-    },
-    {
-      type: 'status',
-      title: 'lighting',
-      data: {
-        value: 32,
-        status: 'on',
-        fieldName: 'total',
-        fieldValue: 43,
-      },
-    },
-  ];
+  public cards: any[];
+  public cardsData: string;
+
+  @ViewChild('cardsData', { static: true }) textarea: ElementRef;
+
+  ngOnInit() {
+    this.cards = cards;
+    this.cardsData = JSON.stringify({ cards });
+  }
+
+  ngAfterViewInit() {
+    this.textarea.nativeElement.addEventListener('ngModelChange', (event) => {
+      try {
+        this.cards = JSON.parse(event);
+      } catch (e) {
+        alert(e);
+      }
+    });
+  }
 }
